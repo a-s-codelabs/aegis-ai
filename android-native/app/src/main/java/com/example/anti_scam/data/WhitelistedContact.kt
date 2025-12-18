2d909026bc7edaf83bda379e1a230b67b8f8805f
shalini-2305 <shalinimunusamy232003@gmail.com>
2025-12-18 11:15:38 +0530

    refactor: removed native files

--- android-native/app/src/main/java/com/example/anti_scam/data/WhitelistedContact.kt
+++
@@ -1,9 +0,0 @@
-package com.example.anti_scam.data
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "whitelisted_contacts")
-data class WhitelistedContact(
-    @PrimaryKey val number: String
-)
--- android-native/app/src/main/java/com/example/anti_scam/ui/dashboard/DashboardViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.anti_scam.ui.dashboard
-
-import androidx.lifecycle.ViewModel
-
-class DashboardViewModel : ViewModel() {
-    // Add relevant logic here in the future
-}
